a63b790923ad1dc7e4be2a2908c8323aa1ce1b09
Rails Udemy Course <gochualex@gmail.com>
2016-05-30 20:14:35 +0000

    Add Stripe, accept payments, update registrations and update tenant

--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,16 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => { 
-    :registrations => "milia/registrations",
+    #:registrations => "milia/registrations",
+    :registrations => "registrations",
     #:confirmations => "milia/confirmations",
     :confirmations => "confirmations",
     :sessions => "milia/sessions", 
     :passwords => "milia/passwords", 
   }
+  
+  match '/plan/edit' => 'tenants#edit', via: :get, as: :edit_plan
+  match '/plan/update' => 'tenants#update', via: [:put, :patch], as: :update_plan
 
 
   #get 'home/index'
